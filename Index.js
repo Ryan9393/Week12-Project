cfb3ca73ffb9547057c9fec33cedd39c2582c14a
Ryan9393 <mryan9393@gmail.com>
2022-08-20 13:30:02 -0400

    added files

---
+++ Index.js
@@ -0,0 +1,5 @@
+// 1.	Create a full CRUD application of your choice. If you can use an existing API, use AJAX to interact with it.
+// However, you do not have to use an API. If you do not use an API, store the entities you will create, read, update, and delete in an array.
+// a.	Use a form to add new entities
+// b.	Build a way for users to update or delete entities
+// c.	Use Bootstrap and CSS to style your project
